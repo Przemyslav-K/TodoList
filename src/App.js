b5cc5803187c2b622254b4b0e938e591e399fb9e
Przemyslav-K <primero.el.dev@gmail.com>
2021-12-19 13:45:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography } from '@material-ui/core';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
@@ -12,25 +12,33 @@ class App extends React.Component {
     }
     this.createTodo = this.createTodo.bind(this)
     this.removeTodo = this.removeTodo.bind(this)
+    this.storeTodos = this.storeTodos.bind(this)
   }
 
   createTodo(todo) {
     this.setState(prevState => {
-      console.log(prevState)
       return {
         ...prevState,
         todos: [todo, ...prevState.todos]
       }
     })
+    this.storeTodos()
   }
   
   removeTodo(id) {
     this.setState(prevState => {
       return {
         ...prevState,
-        todos: prevState.todos.filter(todo => todo.id != id)
+        todos: prevState.todos.filter(todo => todo.id !== id)
       }
     })
+    this.storeTodos()
+  }
+
+  storeTodos() {
+    this.useEffect(() => {
+      localStorage.setItem('todos', this.state.todos)
+    }, [this.state.todos])
   }
 
   render() {
